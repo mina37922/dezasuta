8f70f222b6d862efec7a5d358a4aba95ce2705b5
mina37922 <reo.matu@gmail.com>
2023-02-09 20:07:52 +0900

    work4_修正+js追加

--- work4/js/script.js
+++ work4/js/script.js
@@ -10,3 +10,32 @@ $(function () {
     });
     
   });
+
+// メインビジュアル
+  $('.mainVisual-slider').slick({
+		fade:true,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		speed:1000,
+		infinite: true,
+		dots: true,
+    arrows: false,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        pauseOnDotsHover: false,
+});
+
+
+
+$('.gallery-wrap').slick({
+  arrows: true,
+  autoplay: false,
+  autoplaySpeed: 0,
+  speed: 300,
+  infinite: true,
+  cssEase: 'linear',
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  prevArrow: '<div class="slick-prev"></div>',
+  nextArrow: '<div class="slick-next"></div>',
+});
