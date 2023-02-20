d6479f36ce5a245b40190444ee0e52c9a29ce862
mina37922 <reo.matu@gmail.com>
2023-02-20 16:08:27 +0900

    work5

---
+++ work5/js/script.js
@@ -0,0 +1,55 @@
+$(function () {
+    var pagetop = $('#js-pageTop');
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 500) {
+        pagetop.fadeIn();
+      } else {
+        pagetop.fadeOut();
+      }
+    });
+    
+  });
+
+// メインビジュアル
+  $('.mainVisual-slider').slick({
+		fade:true,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		speed:1000,
+		infinite: true,
+		dots: true,
+    arrows: false,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        pauseOnDotsHover: false,
+});
+
+// ギャラリー
+$('.gallery-wrap').slick({
+  arrows: true,
+  autoplay: false,
+  autoplaySpeed: 0,
+  speed: 300,
+  infinite: true,
+  cssEase: 'linear',
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  prevArrow: '<div class="slick-prev"></div>',
+  nextArrow: '<div class="slick-next"></div>',
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+        slidesToShow: 1,},
+      },
+    ],
+});
+
+// ハンバーガーメニュー
+$(function(){
+  $('.hamburger').on('click', function () {
+      $('.sp-nav').fadeToggle();
+      $('.hamburger').toggleClass('open');
+  });
+  
+});
